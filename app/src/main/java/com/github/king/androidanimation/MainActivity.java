7fec39781c08bf94de83aaec971ab779078e9b32
jinxl <121552497@qq.com>
2018-12-21 15:10:30 +0800

    init commit

---
+++ app/src/main/java/com/github/king/androidanimation/MainActivity.java
@@ -0,0 +1,26 @@
+package com.github.king.androidanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void alpha(View view) {
+    }
+
+    public void scale(View view) {
+    }
+
+    public void rotate(View view) {
+    }
+
+    public void trans(View view) {
+    }
+}
