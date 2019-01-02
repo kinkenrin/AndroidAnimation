7b5f49c047eb0ed0889ae42bfcb521b40586de25
jinxl <121552497@qq.com>
2019-01-02 15:01:58 +0800

    add PointView PropertyValuesHolder

--- app/src/main/java/com/github/king/androidanimation/PointViewAnimActivity.java
+++ app/src/main/java/com/github/king/androidanimation/PointViewAnimActivity.java
@@ -3,26 +3,16 @@ package com.github.king.androidanimation;
 import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.Keyframe;
+import android.animation.LayoutTransition;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.github.king.androidanimation.view.MyPointView;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
 
 public class PointViewAnimActivity extends AppCompatActivity {
 
@@ -40,7 +30,6 @@ public class PointViewAnimActivity extends AppCompatActivity {
         ObjectAnimator animator = ObjectAnimator.ofFloat(mMyPointView, "point", 1, 2, 3, 2, 1, 0);
 
         animator.start();
-
         PropertyValuesHolder point = PropertyValuesHolder.ofInt("backgroundColor", 1, 0);
         ObjectAnimator.ofPropertyValuesHolder(mMyPointView, point);
 
@@ -52,11 +41,13 @@ public class PointViewAnimActivity extends AppCompatActivity {
         animator2.setDuration(1000);
         animator2.start();
 
-
         AnimatorSet animatorSet = new AnimatorSet();
         AnimatorSet.Builder builder = animatorSet.play(animator2);
         builder.with(animator);
         animatorSet.start();
+
+        LayoutTransition layoutTransition = new LayoutTransition();
+        layoutTransition.setAnimator(LayoutTransition.CHANGE_DISAPPEARING,animator);
 
 
 
